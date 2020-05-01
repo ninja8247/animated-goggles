d3fe79cd3f8aceafbe82ee6b76e6681a266a282b
ninja8247 <ninja8247@yahoo.com>
2020-05-01 13:26:48 -0400

    Update Install_Arch_Packages.sh

--- Install_Arch_Packages.sh
+++ Install_Arch_Packages.sh
@@ -5,5 +5,5 @@ pacman -S --needed - < Arch_Base_Packages.txt
 systemctl enable --now dhcpcd.service
 
 git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+
+echo "Install YAY AFTER full installation!!!"
